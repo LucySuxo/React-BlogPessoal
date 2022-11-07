dc08895d002d5477419322a855364dc10ae8df28
Lucy Suxo <lucy.elaine1@yahoo.com.br>
2022-11-07 17:57:36 -0300

    Refatoração do css

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,9 +1,88 @@
 import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Box } from '@mui/material';
+import './Footer.css';
 
 function Footer() {
   return (
     <>
-      <h1> Footer </h1>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid alignItems="center" item xs={12}>
+          <Box className="box1">
+            <Box
+              paddingTop={1}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Typography
+                variant="h5"
+                align="center"
+                gutterBottom
+                className="textos"
+              >
+                Siga-nos nas redes sociais{' '}
+              </Typography>
+            </Box>
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <a
+                href="https://www.facebook.com/generationbrasil"
+                target="_blank"
+              >
+                <FacebookIcon className="redes" />
+              </a>
+              <a
+                href="https://www.instagram.com/generationbrasil/"
+                target="_blank"
+              >
+                <InstagramIcon className="redes" />
+              </a>
+              <a
+                href="https://www.linkedin.com/school/generationbrasil/"
+                target="_blank"
+              >
+                <LinkedInIcon className="redes" />
+              </a>
+            </Box>
+          </Box>
+          <Box className="box2">
+            <Box paddingTop={1}>
+              <Typography
+                variant="subtitle2"
+                align="center"
+                gutterBottom
+                className="textos"
+              >
+                © 2020 Copyright:
+              </Typography>
+            </Box>
+            <Box>
+              <a
+                className="text-decorator-none"
+                target="_blank"
+                href="https://brasil.generation.org"
+              >
+                <Typography
+                  variant="subtitle2"
+                  gutterBottom
+                  className="textos"
+                  align="center"
+                >
+                  brasil.generation.org
+                </Typography>
+              </a>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,58 @@
 import React from 'react';
 import './Home.css';
+import { Typography, Grid, Button } from '@material-ui/core';
+import { Box } from '@mui/material';
 
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="caixa"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="titulo"
+            >
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className="titulo"
+            >
+              expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}></Box>
+            <Button variant="outlined" className="botao">
+              Ver Postagens
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="https://i.imgur.com/H88yIo2.png"
+            alt=""
+            width="500px"
+            height="500px"
+          />
+        </Grid>
+        <Grid xs={12} className="postagens"></Grid>
+      </Grid>
     </>
   );
 }
@@ -18,7 +66,7 @@ export default Home;
 // Linha 5: vai retornar o metodo httml que precisa ser renderizado na tela
 //dentro do return() pode colocar qualquer conteúdo que queira, mas tenha em mente que um componente só pode retornar um elemento
 //se quiser ter mais de um conteúdo na tela, é preciso criar um container
-// Linha 6: forma fragmentada/ abreviada <> que o React usa para definir os elementos/quantos elementos quer que retorne
+// Linha 6: forma fragmentada/ abreviada que o React usa para definir os elementos/quantos elementos quer que retorne
 // Linha 7: inserção do titulo
 // Linha 8: Inserção da imagem/gif para o Home
 //Linha 18: está exportando o componente: export default NOME;
